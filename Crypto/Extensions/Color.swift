2eee2b9d51b3414a11684a8950e4d257647d2ad8
kudzinp <gavrilkoo@gmail.com>
2025-04-03 21:47:30 +0200

    added logos and theme colors

---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 03/04/2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
