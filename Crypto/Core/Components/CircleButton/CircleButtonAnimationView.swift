590d2352c78d70bebc39c1d0820ae67651ccb599
kudzinp <gavrilkoo@gmail.com>
2025-05-26 16:07:56 +0200

    added navigation bar to home view

---
+++ Crypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,27 @@
+//
+//  CircleButtonAnimationView.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 26/05/2025.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    
+    @Binding var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none, value: UUID())
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView(animate: .constant(false))
+        .foregroundColor(.red)
+        .frame(width: 100, height: 100)
+}
---
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,64 @@
+//
+//  HomeView.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 26/05/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(nil, value: UUID())
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Lieve Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(nil, value: UUID())
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(.degrees(showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+    
+}
