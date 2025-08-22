42d4823e0f2dd6d77de141015f3ce4e72952ca4b
kudzinp <gavrilkoo@gmail.com>
2025-08-22 21:58:08 +0200

    added model and view for market statistics

---
+++ Crypto/Core/Components/StatisticView.swift
@@ -0,0 +1,47 @@
+//
+//  StatisticView.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 22/08/2025.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    
+    let stat: StatisticModel
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(stat.title)
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+            Text(stat.value)
+                .font(.headline)
+                .foregroundColor(Color.theme.accent)
+            HStack(spacing: 4) {
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(
+                        Angle(degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180))
+                Text(stat.percentageChange?.asPercentString() ?? "")
+                    .font(.caption)
+                    .bold()
+            }
+            .foregroundColor((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+            .opacity(stat.percentageChange == nil ? 0.0 : 1.0)
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    StatisticView(stat: DeveloperPreview.instance.stat1)
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    StatisticView(stat: DeveloperPreview.instance.stat2)
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    StatisticView(stat: DeveloperPreview.instance.stat3)
+}
