f084779e7791e4dfc96156a757ecf912670da4eb
kudzinp <gavrilkoo@gmail.com>
2025-09-29 22:39:51 +0200

    added segue to PortfolioView

---
+++ Crypto/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 29/09/2025.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text("PORTFOLIO VIEW!!!")
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
