52ecd92aa99b735500a14fcc85f97b544beaeaec
kudzinp <gavrilkoo@gmail.com>
2025-12-30 23:14:08 +0100

    was added updating of portfolio value and coin price

---
+++ Crypto/Utilities/HapticManager.swift
@@ -0,0 +1,19 @@
+//
+//  HapticManager.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 30/12/2025.
+//
+
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+    
+}
