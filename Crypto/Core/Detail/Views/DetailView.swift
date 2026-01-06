302a03c015d3ed496280f9660cee7319745da8d3
kudzinp <gavrilkoo@gmail.com>
2026-01-06 21:09:52 +0100

    added lazy segue to detail view

---
+++ Crypto/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,40 @@
+//
+//  DetailView.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 06/01/2026.
+//
+
+import SwiftUI
+
+struct DetailLoadingView: View {
+    
+    @Binding var coin: CoinModel?
+    
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+            }
+        }
+    }
+    
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        print("Initializing Detail View for \(coin.name)")
+    }
+    
+    var body: some View {
+        Text(coin.name)
+    }
+}
+
+#Preview {
+    DetailView(coin: DeveloperPreview.instance.coin)
+}
