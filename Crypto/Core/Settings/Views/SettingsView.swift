aa478f155db62bcd8973140fd143d43d83dd71e7
kudzinp <gavrilkoo@gmail.com>
2026-01-30 23:46:19 +0100

    settings view

---
+++ Crypto/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,107 @@
+//
+//  SettingsView.swift
+//  Crypto
+//
+//  Created by Pavel Kudin on 30/01/2026.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    let defaultURL = URL(string: "https://www.google.com")!
+    let youtubeURL = URL(string: "https://www.youtube.com")!
+    let coffeeURL = URL(string: "https://www.buymeacoffee.com")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://github.com")!
+    
+    var body: some View {
+        NavigationView {
+            List {
+                unknownIncSection
+                coinGeckoSection
+                developerSection
+                applicationSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
+
+extension SettingsView {
+    
+    private var unknownIncSection: some View {
+        Section(header: Text("UNKNOWN INC.")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made for tracking cryptocurrencies data and saving amount of own funds to the portfolio in a simpliest way.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding()
+            Link("Subscribe on YouTube 🥳", destination: youtubeURL)
+            Link("Support his coffee addiction ☕️", destination: coffeeURL)
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("COINGECKO")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in this app comes from a free API from CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding()
+            Link("Visit CoinGecko 🦎", destination: coingeckoURL)
+        }
+    }
+    
+    private var developerSection: some View {
+        Section(header: Text("DEVELOPER")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was developed by unknown, but very talented person. It uses SwiftUI and is written 100% in Swift. The project benefits from multi-threading, publishers/subscribers and data persistance.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding()
+            Link("Visit Website 💻", destination: personalURL)
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section(header: Text("APPLICATION")) {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+    
+}
