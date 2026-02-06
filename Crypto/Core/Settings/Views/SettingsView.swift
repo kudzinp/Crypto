403068c88e4dd5c60766aaa8cf99b0b8e0cf3e77
kudzinp <gavrilkoo@gmail.com>
2026-02-06 21:18:26 +0100

    updating color theme

--- Crypto/Core/Settings/Views/SettingsView.swift
+++ Crypto/Core/Settings/Views/SettingsView.swift
@@ -17,11 +17,21 @@ struct SettingsView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                unknownIncSection
-                coinGeckoSection
-                developerSection
-                applicationSection
+            ZStack {
+                // background
+                Color.theme.background.ignoresSafeArea()
+                
+                // content
+                List {
+                    unknownIncSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    coinGeckoSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    developerSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    applicationSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                }
             }
             .font(.headline)
             .accentColor(.blue)
